f52bcc4ed17f0877df2a273d36d36e2e0fec8880
ge-hall <ge-hall@outlook.com>
2017-09-10 16:04:18 +1000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  PIXABAY_API_KEY: '6396491-f451c1858f0a5ff45bba47adc',
+  PIXABAY_API_KEY: 'xxxxx',
   PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };
